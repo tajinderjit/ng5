699c5b941088c9fcbc908adc251bd6c8e29dbd00
tajinder.1810@gmail.com <tajinder.1312@gmail.com>
2018-04-16 17:41:44 -0400

    Routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+
 
 
 @Component({
@@ -12,10 +15,16 @@ export class RecipeListComponent implements OnInit {
   
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService ) { }
+  constructor(private recipeService: RecipeService, 
+  	private route: ActivatedRoute,  private router: Router) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  
+  onNewRecipe() {
+  	this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
