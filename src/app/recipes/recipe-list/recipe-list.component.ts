b9a02181d8f2f631256ba0041210b40c8db672ef
tajinder.1810@gmail.com <tajinder.1312@gmail.com>
2018-04-18 11:59:15 -0400

    Forms

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -11,20 +12,30 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService, 
   	private route: ActivatedRoute,  private router: Router) { }
 
   ngOnInit() {
+    this.subscription = this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipes[]) => {
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
   }
 
   
   onNewRecipe() {
   	this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,14 @@
-import { EventEmitter, Injectable } from '@angular/core'
+import { Injectable } from '@angular/core'
+import { Subject } from 'rxjs/Subject';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService{
-	recipeSelected = new EventEmitter<Recipe>();
+	
+	recipesChanged = new Subject<Recipe[]>()	
 
 	private recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 
@@ -39,5 +42,20 @@ export class RecipeService{
 
 	addIngredientsToShoppingList(ingredients: Ingredient[]){
 		this.slService.addIngredients(ingredients);
+	}
+
+	addRecipe(recipe: Recipe){
+		this.recipes.push(recipe);
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	updateRecipe(index: number, newRecipe: Recipe){
+		this.recipes[index] = newRecipe;
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	deleteRecipe(index: number){
+		this.recipes.splice(index, 1);
+		this.recipesChanged.next(this.recipes.slice());
 	}
 }
